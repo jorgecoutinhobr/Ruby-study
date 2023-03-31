35932d24a905d37e6a3100eb0aa82d3a1d52af06
jorge coutinho <jorgecsn@id.uff.br>
2023-03-31 16:24:10 -0300

    teste-commit-laptop

---
+++ 11 - Debugging/the_debugger_and_binding_break_methods.rb
@@ -0,0 +1,16 @@
+require "debug"
+
+candy = "Sour patch kids"
+puts "I love eating #{candy}"
+
+debugger
+
+test = "Ice Tea"
+puts "I love #{test}"
+
+debugger
+
+puts "This is the end of my program"
+
+# commands: continue or c to continue executing 
+# debugger == 
